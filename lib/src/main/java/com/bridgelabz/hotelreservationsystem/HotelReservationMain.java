86ab8b0fce76d1ffb7ed98e70e34ab80c4aa8c6d
srinadh1304 <srinadh1304@gmail.com>
2021-09-17 23:01:15 +0530

    MODIFIED: Validating cheapest hotel for given date range

--- lib/src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationMain.java
+++ lib/src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationMain.java
@@ -1,14 +1,23 @@
 package com.bridgelabz.hotelreservationsystem;
 
-import java.util.ArrayList;
-
-public class HotelReservationMain {
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.*;
+public class HotelReservationMain implements HotelReservationInterface {
 	
 	ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
 	Hotel hotels;
-	public boolean addHotel(String hotelName,int rate)
-	{
-		hotels=new Hotel(hotelName,rate);
+	@Override
+	public boolean addHotel(String name, double rate) {
+		hotels=new Hotel(name,rate);
 		return this.hotelList.add(hotels);
 	}
+	public Hotel getCheapestHotel(LocalDate date1, LocalDate date2) {
+		Period period = Period.between(date1, date2);
+		int numOfDays = period.getDays();
+		return hotelList.stream().min((h1,h2) -> h1.getPrice(numOfDays).compareTo(h2.getPrice(numOfDays))).orElse(null);
+	}
+	
+	
+
 }
--- lib/src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
+++ lib/src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
@@ -1,5 +1,9 @@
 package com.bridgelabz.hotelreservationsystem;
 
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDate;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,11 +12,22 @@ public class HotelReservationTest {
 	public void givenHotel_Added_ShoulReturnTrue()
 	{
 		HotelReservationMain hotelReservation = new HotelReservationMain();
-		boolean hotel1 = hotelReservation.addHotel("Lakewood",110);
+		boolean hotel1 = hotelReservation.addHotel("Lakewood",110.0);
 		Assert.assertTrue(hotel1);
-		boolean hotel2 = hotelReservation.addHotel("Bridgewood",160);
+		boolean hotel2 = hotelReservation.addHotel("Bridgewood",160.0);
 		Assert.assertTrue(hotel2);
-		boolean hotel3 = hotelReservation.addHotel("Ridgewood", 90);
+		boolean hotel3 = hotelReservation.addHotel("Ridgewood", 90.0);
 		Assert.assertTrue(hotel3);
+	}
+	
+	@Test
+	public void given_Start_End_Date_ShouldReturn_CheapestPrice() throws Exception{
+		HotelReservationMain hotelReservation = new HotelReservationMain();
+		hotelReservation.addHotel("Bridgewood", 100);
+		hotelReservation.addHotel("Lakewood", 200);
+		LocalDate startDate = LocalDate.of(2021, 9, 10);
+		LocalDate endDate = LocalDate.of(2021, 9, 11);
+		Hotel hotel = hotelReservation.getCheapestHotel(startDate, endDate);
+		assertEquals("Bridgewood", hotel.getName());
 	}
 }
