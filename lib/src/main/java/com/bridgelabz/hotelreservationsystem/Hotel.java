7a27a3f72ac8886382a8c449e8dad1f13471dac7
srinadh1304 <srinadh1304@gmail.com>
2021-09-18 07:21:41 +0530

    MODIFIED: added reward customers weekday and weekend prices constructor

--- lib/src/main/java/com/bridgelabz/hotelreservationsystem/Hotel.java
+++ lib/src/main/java/com/bridgelabz/hotelreservationsystem/Hotel.java
@@ -4,7 +4,10 @@ public class Hotel {
 	private String name;
 	private double weekendPrice;
 	private double weekdayPrice;
+	private double rewardWeekdayPrice;
+	private double rewardWeekendPrice;
 	private int rating;
+
 
 	public Hotel(String name, double weekendPrice, double weekdayPrice, int rating) {
 		super();
@@ -13,7 +16,16 @@ public class Hotel {
 		this.weekdayPrice = weekdayPrice;
 		this.rating = rating;
 	}
-
+	public Hotel(String name, double weekendPrice, double weekdayPrice, double rewardWeekdayPrice,
+			double rewardWeekendPrice, int rating) {
+		super();
+		this.name = name;
+		this.weekendPrice = weekendPrice;
+		this.weekdayPrice = weekdayPrice;
+		this.rewardWeekdayPrice = rewardWeekdayPrice;
+		this.rewardWeekendPrice = rewardWeekendPrice;
+		this.rating = rating;
+	}
 	public String getName() {
 		return name;
 	}
@@ -48,7 +60,10 @@ public class Hotel {
 	public Double getPrice(int numOfWeekdays, int numOfWeekends) {
 		return weekdayPrice*numOfWeekdays + weekendPrice*numOfWeekends;
 	}
-	
+	public double getPriceForRewardCustomers(int numberOfWeekdays, int numberOfWeekends) 
+	{
+		return rewardWeekdayPrice*numberOfWeekdays +rewardWeekendPrice*numberOfWeekends;
+	}
 	@Override
 	public String toString() {
 		return "Hotel Name: "+this.name+" Week day Price: "+this.weekdayPrice+" Week end Price: "+this.weekendPrice+" Rating: "+this.rating; 
